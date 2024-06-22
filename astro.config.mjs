9fceee97e31bc370e88b19cd459e6e36ebcc3174
Juan Q. <>
2024-06-22 01:14:40 -0500

    Add workflow to deploy to GH Pages
    Includes example file from Astro docs for easy deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jgqh.github.io',
   integrations: [tailwind(), mdx()],
   i18n: {
     defaultLocale: 'es',
